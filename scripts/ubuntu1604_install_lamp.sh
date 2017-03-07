530f1dc8f94e40bde1f2847be5cdd1aca9d75dbe
Stephen Lang <saftslang@gmail.com>
2017-03-07 16:16:16 -0500

    Updating curl command

--- scripts/ubuntu1604_install_lamp.sh
+++ scripts/ubuntu1604_install_lamp.sh
@@ -268,7 +268,7 @@ systemctl restart apache2
 txtbld=$(tput bold)
 lightblue=`tput setaf 6`
 nc=`tput sgr0`
-real_ip=`curl --silent icanhazip.com 2>&1`
+real_ip=`curl --silent -4 icanhazip.com 2>&1`
 
 # Generate setup report
 
